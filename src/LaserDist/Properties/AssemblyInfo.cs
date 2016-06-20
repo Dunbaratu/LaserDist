4e6e7e2befd07281070548a4429f5c119615e019
Dunbaratu <madings@gmail.com>
2016-06-20 01:58:07 -0500

    Moved AssemblyInfo.cs into source directory.
    I have no idea why SharpDevelop stuck AssemblyInfo by
    default  into the obj/ folder, but it did.  I moved it
    to where it belongs, with all the other source code.

---
+++ src/LaserDist/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LaserDist")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LaserDist")]
+[assembly: AssemblyCopyright("Copyright ©2016 Steven Mading")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyFileVersion("0.9.2.0")]
+[assembly: AssemblyVersion("0.9.2.0")]
+[assembly: KSPAssembly("LaserDist", 0, 20)]
